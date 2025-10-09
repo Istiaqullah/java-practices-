b3c65654bf1b9cb0da100f44652f20692ced3423
Abul Hasnat Mohammad Istiaqullah <mohammadistiaqullah@gmail.com>
2025-10-09 20:43:37 +0600

    module update(9/10/25)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,16 @@ module CT1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.graphics;
+    requires java.base;
+    requires java.logging;
+    requires java.sql;
 
+    opens messenger.client to javafx.fxml;
+    exports messenger.client;
+
+    opens messenger.server to javafx.fxml;
+    exports messenger.server;
 
     opens CT1 to javafx.fxml;
     exports CT1;
